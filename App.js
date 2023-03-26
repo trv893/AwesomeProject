618a8a2ff3173ccc6135bcb2ebe878162370f0bd
trv893 <trevorhilimire@gmail.com>
2023-03-26 00:22:46 -0400

    formatting

--- App.js
+++ App.js
@@ -19,19 +19,19 @@ const windowHeight = Dimensions.get('window').height;
  */
 const App = () => {
   // --- State Variables ---
-  
+
   // @type {Array} shiftData will contain an array of shift assignment objects with the following structure: [{ "shiftAssignmentId": 4988, "userId": 2, "shiftId": 1, "dateAssigned": "2023-03-13T00:00:00", "sectionId": 5, "releasedByUser": false, "dayId": 2, "section": "5", "assignee": "Aleesha Johnson", "releaseByUserId": 0, "shiftName": "AM"}, ...]
   const [shiftData, setShiftData] = useState([]);
-  
+
   // @type {Boolean} isLoading is true when the app is fetching data from the API, and false otherwise
   const [isLoading, setIsLoading] = useState(true);
-  
+
   // @type {Date} The date to start the schedule from
   const [startDate, setStartDate] = useState(new Date());
-  
+
   // @type {Date} The date to end the schedule at
   const [endDate, setEndDate] = useState();
-  
+
   // @type {String} Error message string if API call fails
   const [error, setError] = useState(null);
 
@@ -39,7 +39,7 @@ const App = () => {
   const userId = 5;
 
   // --- Memoized Props ---
-  
+
   // Create a memoized object of props to pass to WeeklySchedule component
   const weeklyScheduleProps = useMemo(
     () => ({
@@ -51,7 +51,7 @@ const App = () => {
   );
 
   // --- API Call ---
-  
+
   useEffect(() => {
     setIsLoading(true); // set loading to true when useEffect runs
 
@@ -81,56 +81,56 @@ const App = () => {
   };
 
   // --- Render ---
-  
+
   // Render the ErrorScreen component if an error occurs during API call
   if (error) {
     return <ErrorScreen message={error} on
-    Retry={() => fetchData()} />;
+      Retry={() => fetchData()} />;
   }
-  
+
   return (
-  <View style={styles.container}>
-  {/* Render the ActivityIndicator if the data is loading, otherwise render the WeeklySchedule component */}
-  {isLoading ? (
-  <View style={styles.loader}>
-  <ActivityIndicator size="large" color="#1e90ff" />
-  <Text style={styles.loadingText}>Loading...</Text>
-  </View>
-  ) : (
-  <WeeklySchedule style={styles.weeklySchedule}
-  {...weeklyScheduleProps}
-  />
-  )}
-  </View>
+    <View style={styles.container}>
+      {/* Render the ActivityIndicator if the data is loading, otherwise render the WeeklySchedule component */}
+      {isLoading ? (
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" color="#1e90ff" />
+          <Text style={styles.loadingText}>Loading...</Text>
+        </View>
+      ) : (
+        <WeeklySchedule style={styles.weeklySchedule}
+          {...weeklyScheduleProps}
+        />
+      )}
+    </View>
   );
-  };
-  
-  const styles = StyleSheet.create({
+};
+
+const styles = StyleSheet.create({
   container: {
-  fontSize: 16,
-  flex: 1,
-  width: windowWidth,
-  height: windowHeight,
-  //maxWidth: "798px",
-  marginHorizontal: windowWidth * 0.02, // set margin of 5% of the device width on the left and right sides
-  marginTop: windowHeight * 0.1, // set margin of 2% of the device height at the top
-  marginBottom: windowHeight * 0.04, // set margin of 2% of the device height at the bottom
-  backgroundColor: "white",
-  justifyContent: "center",
-  alignItems: "center",
+    fontSize: 16,
+    flex: 1,
+    width: windowWidth,
+    height: windowHeight,
+    //maxWidth: "798px",
+    marginHorizontal: windowWidth * 0.02, // set margin of 5% of the device width on the left and right sides
+    marginTop: windowHeight * 0.1, // set margin of 2% of the device height at the top
+    marginBottom: windowHeight * 0.04, // set margin of 2% of the device height at the bottom
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
   },
   loader: {
-  flex: 1,
-  justifyContent: "center",
-  alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   loadingText: {
-  marginTop: 10,
+    marginTop: 10,
   },
   weeklySchedule: {
-  justifyContent: "center",
-  alignItems: "center",
+    justifyContent: "center",
+    alignItems: "center",
   }
-  });
-  
-  export default App;
+});
+
+export default App;
